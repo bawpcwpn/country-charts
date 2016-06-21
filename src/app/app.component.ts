fa2574361b5e54fa57da6ba682dbcff4abdaf67a
Thomas Huxley <thomas.huxley90@gmail.com>
2016-06-21 10:09:23 +1000

    Import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,9 @@
-import {Component, OnInit} from "@angular/core";
-import {RouteConfig, RouterLink, ROUTER_DIRECTIVES} from "@angular/router-deprecated";
-
-import {TaskListComponent} from "./todo/components/task-list.component";
-import {AboutComponent} from "./about/components/about.components";
+import {Component} from "@angular/core";
 
 @Component({
-    selector: "app",
+    selector: 'app',
     templateUrl: "./app/app.html",
-    directives: [TaskListComponent, AboutComponent, RouterLink, ROUTER_DIRECTIVES]
 })
-@RouteConfig([
-    {path: '/tasks', component: TaskListComponent, as: 'TaskList'},
-    {path: '/about', component: AboutComponent, as: 'About'}
-])
-export class AppComponent implements OnInit {
-    ngOnInit() {
-        console.log("Application component initialized ...");
-    }
+export class AppComponent {
+
 }
--- src/app/main.ts
+++ src/app/main.ts
@@ -3,11 +3,5 @@
 
 import {AppComponent} from "./app.component";
 import {bootstrap} from "@angular/platform-browser-dynamic";
-import {provide} from "@angular/core";
-import {LocationStrategy, HashLocationStrategy} from "@angular/common";
-import {ROUTER_PROVIDERS} from "@angular/router-deprecated";
 
-bootstrap(AppComponent, [
-    ROUTER_PROVIDERS,
-    provide(LocationStrategy, {useClass: HashLocationStrategy})
-]);
+bootstrap(AppComponent);
